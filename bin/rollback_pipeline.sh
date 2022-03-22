01cca672d3a0c9ab89ff80b1a467b0a455e6ca4d
Xavi Mañá <xavier.manya@gmail.com>
2022-03-22 16:51:45 +0100

    rb witouth bq

--- bin/rollback_pipeline.sh
+++ bin/rollback_pipeline.sh
@@ -1,5 +1,36 @@
 # !/bin/bash
+###Getting keys:
+keys_file=gcp.keys; #swap it for dummy.keys
+declare -a info=$(< $keys_file);
+delimiter=':';
+declare -A dict;
+for element in ${info[@]};
+do
+ dict[$(echo $element | cut -d $delimiter -f 1)]=$(echo $element | cut --complement -d $delimiter -f 1);
+done
+###Processing options:
+target=datasets/;
+args=$(getopt -a -o t:n: --long target:,name: -- "$@");
+
+if [[ $? -ne 0 ]]; then
+ echo "Invalid option";
+ exit 2;
+fi
+
+eval set -- "$args"
+while :
+do
+    case $1 in
+     -n | --name) name=$2; shift 2;;
+     -t | --target) target=$2; shift 2;;
+     --) shift; break;;
+     *) echo "Invalid option $1"; exit 2;;
+    esac
+done
 #move files back to unprocessed folder
+find "$target$name/original" -type f -exec mv -f {} "$target/unprocessed/" \;
 #remove BQ dataset
 #remove bucket
+gsutil rm -r "${dict[parent-bucket]}$name/";
 #rm local directory
+rm -r "$target/$name";
